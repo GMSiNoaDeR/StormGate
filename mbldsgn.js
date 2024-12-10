d7dea3942bb1818a4aa1bc4437cbe647da6a2275
GMSiNoaDeR <sinoaderr@protonmail.com>
2024-12-11 01:12:35 +0300

    Add files via upload

--- mbldsgn.js
+++ mbldsgn.js
@@ -1,14 +1,16 @@
-// Добавление кнопки меню и управление выпадающим меню
+// Добавление кнопки меню и управление выпадающим меню (только для мобильных устройств)
 document.addEventListener("DOMContentLoaded", function () {
-    const menuButton = document.createElement("button");
-    menuButton.classList.add("menu-button");
-    menuButton.textContent = "Меню";
+    if (window.matchMedia("(max-width: 768px)").matches) { // Проверка ширины экрана
+        const menuButton = document.createElement("button");
+        menuButton.classList.add("menu-button");
+        menuButton.textContent = "Меню";
 
-    const header = document.querySelector("header");
-    header.appendChild(menuButton);
+        const header = document.querySelector("header");
+        header.appendChild(menuButton);
 
-    const nav = header.querySelector("nav");
-    menuButton.addEventListener("click", function () {
-        nav.classList.toggle("active");
-    });
+        const nav = header.querySelector("nav");
+        menuButton.addEventListener("click", function () {
+            nav.classList.toggle("active");
+        });
+    }
 });
