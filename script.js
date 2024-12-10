1aec8fcb214bc6581a3a49aca92c24501433d29e
GMSiNoaDeR <sinoaderr@protonmail.com>
2024-12-10 19:37:09 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,41 @@
-function openMenu() {
-    document.getElementById("side-menu").style.width = "250px";
+// Открыть меню
+function toggleMenu() {
+    var menu = document.getElementById("side-menu");
+    var button = document.getElementById("menu-toggle");
+
+    if (menu.style.width === "250px") {
+        closeMenu();
+    } else {
+        menu.style.width = "250px";
+        button.classList.add("open");
+    }
 }
+
+// Закрыть меню
 function closeMenu() {
-    document.getElementById("side-menu").style.width = "0";
+    var menu = document.getElementById("side-menu");
+    var button = document.getElementById("menu-toggle");
+
+    menu.style.width = "0";
+    button.classList.remove("open");
 }
+
+// Открыть меню при наведении мышки
+document.getElementById("side-menu").addEventListener("mouseover", function() {
+    var menu = document.getElementById("side-menu");
+    var button = document.getElementById("menu-toggle");
+    if (menu.style.width === "0px") {
+        menu.style.width = "250px";
+        button.classList.add("open");
+    }
+});
+
+// Закрыть меню, если курсор уходит с панели
+document.getElementById("side-menu").addEventListener("mouseleave", function() {
+    var menu = document.getElementById("side-menu");
+    var button = document.getElementById("menu-toggle");
+    if (menu.style.width === "250px") {
+        menu.style.width = "0";
+        button.classList.remove("open");
+    }
+});
